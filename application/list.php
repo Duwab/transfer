00140cd669110a62ef8fbdcd4b8879b6ae7486c2
Administrator <admin@duwab.com>
2014-09-13 09:37:34 +0200

    init

---
+++ application/list.php
@@ -0,0 +1,12 @@
+<?php  defined('SYSPATH') or die('No direct script access.');
+?>
+<!DOCTYPE html>
+<head>
+	<title>Duwab/transfer</title>
+	<script type="text/javascript" async="" src="/assets/js/script.js"></script>
+	<link href="/assets/css/style.css" media="all" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1>Nous transférons</h1>
+	<table></table>
+</body>
